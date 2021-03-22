76e68333931db8d7c3ba4110210f39949778480a
rmarita <rmarita@palcnetworks.com>
2021-03-22 11:26:41 +0530

    changes in testng xml file

--- src/Script/AmazonTitle.java
+++ src/Script/AmazonTitle.java
@@ -10,13 +10,13 @@ import generic.WebBase;
 
 public class AmazonTitle extends WebBase {
 	
-	@Test
+	@Test(priority=1)
 	public void amazonlogin() throws IOException, InterruptedException {
 	
 		driver.get(" https://www.amazon.in/");
 		Thread.sleep(3000);
 	}
-	@Test
+	@Test(priority=2)
 		public void signup() throws IOException, InterruptedException {
 		
 		String expectedTitle = "Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in";
