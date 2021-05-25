00e61e26619ed6d05cb28df29f7e579a26146c9e
rmarita <rmarita@palcnetworks.com>
2021-05-25 19:17:27 +0530

    updated webbase code added headless chrome browser
    Signed-off-by: rmarita <rmarita@palcnetworks.com>

--- src/generic/WebBase.java
+++ src/generic/WebBase.java
@@ -1,48 +1,51 @@
 package generic;
 
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+;
 
-public class WebBase implements AutoConst{
+public class WebBase implements AutoConst {
+	public static HashMap<String, WebDriver> driverObjMap = new HashMap<String, WebDriver>();
 
-	static
-	{
-
-		System.setProperty(CHROME_KEY,CHROME_VALUE);	
+	static {
+		System.setProperty(CHROME_KEY, CHROME_VALUE);
 
 	}
-
 	public static WebDriver driver;
-	@BeforeClass
-	public void OpenApplication()
-	{
-		try
-		{
-				/*ChromeOptions chromeOptions = new ChromeOptions();
-			chromeOptions.addArguments("--headless");	
-			driver = new ChromeDriver(chromeOptions);*/	
-			
-			driver = new ChromeDriver();
-			driver.manage().window().maximize();
-			Thread.sleep(2000);
 
-		} catch(Exception e)
-		{
-			Reporter.log("Server down",true);
+	@BeforeClass
+	public void openApplication() throws InterruptedException {
+		try {
+			// for headless chrome browser make it as true
+			runHeadless(false);
+			Thread.sleep(2000);
+		} catch (Exception e) {
+			Reporter.log("Server down", true);
 			System.out.println(e);
 		}
 	}
 
-	@AfterClass
-	public void closeApplication() throws InterruptedException
-	{
-		Thread.sleep(2000);
-		driver.quit();
-	}	
+	private void runHeadless(boolean headless) {
+		if (headless) {
+			ChromeOptions chromeOptions = new ChromeOptions();
+			chromeOptions.addArguments("--headless");
+		} else {
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		}
+	}
 
+	@AfterClass
+	public void closeApplication() {
+		driver.quit();
+	}
 
 }
