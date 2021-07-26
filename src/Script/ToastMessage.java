e29b0be5393fa2b2f80072a7371c840255f51c45
rmarita <rmarita@palcnetworks.com>
2021-07-26 19:02:08 +0530

    updated pom.xml file

--- src/Script/ToastMessage.java
+++ src/Script/ToastMessage.java
@@ -11,7 +11,8 @@ public class ToastMessage extends WebBase {
 	public static void verifytoast() throws InterruptedException {
 
 		driver.get("https://codeseven.github.io/toastr/demo.html");
-
+		Thread.sleep(1000);
+		driver.findElement(By.id("showtoast")).click();
 		String toastMessage = driver.findElement(By.className("toast-message")).getText();
 		System.out.println("Title of the toast message is:" + " " + toastMessage);
 
